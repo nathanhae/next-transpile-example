dc0ab3f7f125351624fafaa817d71b8956008b2a
Nathan Harper <nathan.harper@aenetworks.com>
2024-05-22 09:37:06 -0400

    add task

--- test.js
+++
@@ -1,7 +0,0 @@
-const { GetServerSideProps } = require('next');
-const fart = (...args) => {
-  console.log(...args);
-  console.log(GetServerSideProps);
-  const neeb = {args};
-  return {...args, foo:'bar'};
-};
